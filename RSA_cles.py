e4e63a43b410040067d6e8fb2579da6d7b753b21
Lucaslgl <100415269+Lucaslgl0@users.noreply.github.com>
2022-05-10 17:17:56 +0000

    Update RSA_cles.py

--- RSA_cles.py
+++ RSA_cles.py
@@ -9,9 +9,9 @@ from operations_liste import (
 
 # Choix des nombres premiers et de l'exposant
 
-nombre_premier1 = nombre_vers_liste(991)  # 10000005143
-nombre_premier2 = nombre_vers_liste(727)  # 10000018097
-exposant = [7]
+nombre_premier1 = nombre_vers_liste(10000005143)
+nombre_premier2 = nombre_vers_liste(10000018097)
+exposant = [6,5,5,3,7]
 
 # Création des cles
 
@@ -20,7 +20,7 @@ phi_n = multiplication(
 )
 if PGCD(exposant, phi_n) != [1]:
     print(
-        "e n'est pas premier avec phi_n il faut changer soit e soit un nombre premier"
+        "e n'est pas premier avec phi_n il faut changer soit l'exposant, soit les nombres premiers"
     )
 n = multiplication(nombre_premier1, nombre_premier2)
 d = euclide_etendue(exposant, phi_n)[1]
